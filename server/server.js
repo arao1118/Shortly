3af4cf30a448e8d03833c38c04a081f67d5f0ea9
arao1118 <abhayrao1118@gmail.com>
2026-09-16 22:10:59 +0530

    Initial Commit

--- server/server.js
+++ server/server.js
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
-//import cors from "cors";
 
 import authRoutes from "../routes/authRoutes.js";
 import urlRoutes from "../routes/urlRoutes.js";
@@ -13,10 +12,6 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-//app.use(cors({
-//  origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
-//  credentials: true,
-//}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/url", urlRoutes);
