d06a7336e644bac984655f97b2e0b96ef9273a69
arao1118 <abhayrao1118@gmail.com>
2026-09-21 01:20:59 +0530

    featuer: add pagination

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -2,11 +2,12 @@ import urlModel from "../models/urlModel.js";
 
 export const getAllURL = async (req, res) => {
 
+  const { page, page_size } = req.query;
   try {
 
-    const userData = await urlModel.find({ user: req.user._id });
+    const userData = await urlModel.find({ user: req.user._id }).sort({ createdAt: -1 }).skip((page - 1) * page_size).limit(page_size);
 
-    if (!userData || userData.length == 0) return res.status(400).send({ message: false, message: `User has not created any short links` });
+    if (!userData || userData.length === 0) return res.status(400).send({ message: false, message: `User has not created any short links` });
 
     return res.status(200).json(userData);
 
